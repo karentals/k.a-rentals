5568dbe54c4573b79796a893382ee68c0ebb1fbc
karentals <kladolphus@gmail.com>
2025-09-05 14:44:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Mobile menu toggle
+const burger = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-menu');
+if (burger) {
+  burger.addEventListener('click', () => {
+    mobileMenu.classList.toggle('open');
+  });
+}
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(a => {
+  a.addEventListener('click', e => {
+    const href = a.getAttribute('href');
+    if (href && href.length > 1) {
+      e.preventDefault();
+      document.querySelector(href)?.scrollIntoView({ behavior: 'smooth' });
+      mobileMenu?.classList.remove('open');
+    }
+  });
+});
